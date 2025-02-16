e33ece7628137562e0f53f6d53bc2614b5be46a1
Alrick VINCENT <alrick.vincent@gmail.com>
2025-02-15 18:12:41 -0800

    Launch Indicator added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,6 +8,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
 
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -35,6 +36,7 @@ public class BallController : MonoBehaviour
         isBallLaunched = true;
         transform.parent = null;
         ballRB.isKinematic = false;
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
